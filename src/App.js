e89c9891b4d4706d1b6cec49c5f9559bebb13824
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-09-23 15:47:27 +0530

    Accordion completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordian from './components/Accordion';
+import Search from "./components/Search";
 
-function App() {
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front-end Javascript Framework?',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favortive JS Library among engineers?',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use Reat by creating components'
+  }
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      {/* <Accordian items={items} /> */}
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
